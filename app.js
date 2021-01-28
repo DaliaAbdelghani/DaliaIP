f761bbff5b0478b656e9e96f487679cd01136bb1
DaliaAbdelghani <engdna87@gmail.com>
2021-01-28 14:47:11 +0200

    new JS code

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var question = confirm("Are you an inventor?"); // true false
 var answer;
 
@@ -6,16 +7,36 @@ else { answer = " It's okay you are great. Read more about Intellectual property
 
 alert(answer);
 
-
 var noofbooks = prompt("How many books you read monthly?");
 var offer;
 
 if (noofbooks >= 6) { offer = "Great! you have one month free registration and 10% discount on our services"; }
 else if (noofbooks >= 1 && noofbooks < 6) { offer = "Good. you have 5% discount on our services"; }
 else if (noofbooks < 1) { offer = "You are welcome. kindly register";}
-
-
 alert(offer);
 
 document.write(offer);
 
+
+var fav = prompt("Do you want book or newspaper");
+
+while(fav !== "book" && fav !=="newspaper")
+{
+fav = prompt("Do you want book or newspaper")
+}
+
+var itemorder =" ";
+
+if (fav === "book") {
+itemorder='<img src="images/book.jpg"/>';
+} else if (fav === "newspaper") {itemorder='<img src="images/newspaper.jpg"/>'} 
+
+var nooforder = prompt("How many do you want?")
+var result=" ";
+
+for (var i=0; i<nooforder; i++)
+{
+result = result + itemorder;
+}
+
+document.write(result);
